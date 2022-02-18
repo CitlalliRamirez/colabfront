519e24e1545a48890cad430561be5e48bb66566d
CitlalliRamirez <citlalli.bramirez@gmail.com>
2022-02-18 14:15:02 -0600

    h

--- src/components/AgregarChat.vue
+++ src/components/AgregarChat.vue
@@ -533,7 +533,7 @@ export default {
             that.guardarChat()
           }
           else{
-            setTimeout( () => {return}, 5000 )
+            setTimeout( () => {return}, 9000 )
           }
           return
         }
